a64aa177fe9eddce1862f957bdd78ccf15390476
Ccccx159 <35327600+Ccccx159@users.noreply.github.com>
2020-03-12 19:02:19 +0800

    Create demo_func.h

---
+++ inc/demo_func.h
@@ -0,0 +1,19 @@
+#ifndef _DEMO_FUNC_H
+#define _DEMO_FUNC_H_
+
+#include "01_Exordium/status.h"
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//顺序表的测试接口函数
+void test_SequenceList_demo();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // !1
