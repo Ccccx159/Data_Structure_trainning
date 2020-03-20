4dc9102dd27c473ca2a92641e651eb9cdcecbbf9
Ccccx159 <35327600+Ccccx159@users.noreply.github.com>
2020-03-20 14:54:21 +0800

    Update SinglyLinkedList.h

--- inc/02_LinearTable/02_SinglyLinkedList/SinglyLinkedList.h
+++ inc/02_LinearTable/02_SinglyLinkedList/SinglyLinkedList.h
@@ -9,16 +9,20 @@ typedef int ElemType_int;
 typedef struct linked_list_node_tag {
   ElemType_int data;
   struct linked_list_node_tag *next;
-} linked_list_node, plinked_list_node;
+} linked_list_node, *plinked_list_node;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 // 初始化单链表（带头节点）
-Status init_linked_list(plinked_list_node L);
+Status init_linked_list(plinked_list_node *L);
 
 // 清空单链表（保留头节点）
 Status clear_linked_list(plinked_list_node L);
 
 // 销毁单链表（不保留头节点）
-Status destroy_linked_list(plinked_list_node L);
+Status destroy_linked_list(plinked_list_node *L);
 
 // 判断单链表是否为空
 Status empty_linked_list(plinked_list_node L);
@@ -47,12 +51,17 @@ Status insert_linked_list(plinked_list_node L, int local, ElemType_int e);
 Status delete_linked_list(plinked_list_node L, int local, ElemType_int *e);
 
 // 遍历访问单链表
-void traverse_linked_list(plinked_list_node L);
+Status traverse_linked_list(plinked_list_node L);
 
 // 头插法创建单链表
-Status create_linked_list_h(plinked_list_node L);
+Status create_linked_list_h(plinked_list_node *L,
+                            std::vector(ElemType_int> list);
 
 // 尾插法创建单链表
-Status create_linked_list_t(plinked_list_node L);
+Status create_linked_list_t(plinked_list_node *L,
+                            std::vector(ElemType_int> list);
 
+#ifdef __cplusplus
+}
+#endif
 #endif
