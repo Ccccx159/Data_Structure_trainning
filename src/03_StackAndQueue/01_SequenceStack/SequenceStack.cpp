804a360fdca551b84d36011233e2e983636f13ce
Ccccx <734751044@qq.com>
2020-04-25 02:02:25 +0800

    顺序栈bug修复：push重分配内存部分逻辑存在严重错误；realloc函数存在指针漂移。已修复

--- src/03_StackAndQueue/01_SequenceStack/SequenceStack.cpp
+++ src/03_StackAndQueue/01_SequenceStack/SequenceStack.cpp
@@ -64,9 +64,10 @@ Status push_stack_sq(p_sq_stack_t stk, elemType_sq_stack e) {
   if (stk->stack_len == stk->stackSize) {
     stk->base = (elemType_sq_stack *)realloc(
         stk->base,
-        (STACK_INIT_SIZE + 1 + STACK_INCREMENT) * sizeof(elemType_sq_stack));
+        (stk->stackSize + 1 + STACK_INCREMENT) * sizeof(elemType_sq_stack));
     check_ptr(stk->base);
-    stk->stackSize = STACK_INIT_SIZE + STACK_INCREMENT;
+    stk->top = stk->base + stk->stack_len;
+    stk->stackSize = stk->stackSize + STACK_INCREMENT;
   }
   memcpy(stk->top, &e, sizeof(elemType_sq_stack));
   stk->top++;
@@ -82,24 +83,24 @@ Status pop_stack_sq(p_sq_stack_t stk, elemType_sq_stack *e) {
     return ERROR;
   }
   (*e) = *(--stk->top);
-  // memset(stk->top, 0, sizeof(elemType_sq_stack));
+  memset(stk->top, 0, sizeof(elemType_sq_stack));
   stk->stack_len--;
 
   return OK;
 }
 
 // 遍历
-Status traverse_stack_sq(sq_stack_t stk) {
+Status traverse_stack_sq(sq_stack_t stk, visit vfunc) {
   check_ptr(stk.base);
   if (0 == stk.stack_len) {
     std::cout << "stack is Empty!..." << std::endl;
     return ERROR;
   }
 
-  // for (elemType_sq_stack *tmp = stk.top - 1; tmp >= stk.base;) {
-  //   std::cout << *tmp << " ";
-  //   tmp--;
-  // }
+  for (elemType_sq_stack *tmp = stk.top - 1; tmp >= stk.base;) {
+    vfunc(*tmp);
+    tmp--;
+  }
 
   return OK;
 }
